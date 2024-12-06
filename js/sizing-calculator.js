233cfc624d0431c607cb7a9eaf8361305866039a
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2024-12-06 14:35:45 +0100

    feat: add dynamic form switching funcionality

--- js/sizing-calculator.js
+++ js/sizing-calculator.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   const calculatorType = document.getElementById('calculator-type');
-  const calculatorForm = document.getElementById('calculator-form');
+  const calculatorForms = document.querySelectorAll('.calculator-form');
 
   calculatorType.addEventListener('change', function () {
-    if (calculatorType.value === '0') {
-      calculatorForm.style.display = 'none';
-    } else {
-      calculatorForm.style.display = 'block';
+    const selectedValue = calculatorType.value;
+
+    calculatorForms.forEach((form) => {
+      form.style.display = 'none';
+    });
+
+    if (selectedValue === '0') {
+      return;
+    }
+
+    const formToShow = document.getElementById(selectedValue);
+
+    if (formToShow) {
+      formToShow.style.display = 'block';
     }
   });
 });
